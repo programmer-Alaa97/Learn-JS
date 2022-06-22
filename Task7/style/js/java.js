1b55f260e5c37a7747cd6536f071f490d35e1144
alaa <alaa97programmer@gmail.com>
2022-06-22 14:56:46 +0300

    task7

---
+++ Task7/style/js/java.js
@@ -0,0 +1,8 @@
+let a= "Alaa";
+
+console.log (a.charAt(0).toLowerCase());
+console.log(a[0].toLowerCase()); 
+console.log(a.slice(0,1).toLowerCase());
+console.log(a.substring(0,1).toLowerCase());
+console.log(a.substr(0,1).toLowerCase());
+console.log(a.charAt(0).toLowerCase());
